cd4c6bf247f218293db290c9deaae3d7165171f7
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2022-08-12 13:17:53 +0530

    connection to mongoDB

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { initializeConnection } = require("./database/databaseConnection");
+
+app.use(express.json());
+app.use(cors());
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = 8080;
+initializeConnection();
+
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`listening to port 8080`);
+});
